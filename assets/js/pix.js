d3bf3a493387a04e0ea45be12667559f6b315d5b
Pedro <pedrohenriqueribeirodearaujo01@gmail.com>
2022-12-05 22:10:28 -0300

    enable copying pix on qr code

--- assets/js/pix.js
+++ assets/js/pix.js
@@ -1,11 +1,13 @@
-function copyPixToClipboard(event) {
+function copyPixToClipboard() {
     const pix = "00020126430014br.gov.bcb.pix0114047492980001980203Pix5204000053039865802BR5925PRIMEIRA IGREJA BATISTA R6007AQUIRAZ622905255fIkxzEdCnaRg2hHvkdx9lZS76304B0DF";
 
-    event.target.innerText = "Pix copiado!";
-    event.target.className = "copied";
+    copyPixButton.innerText = "Pix copiado!";
+    copyPixButton.className = "copied";
     navigator.clipboard.writeText(pix);
 }
 
 const copyPixButton = document.getElementById("copy-pix");
+const qrCodeImg = document.getElementById("qr-code");
 
-copyPixButton.addEventListener("click", copyPixToClipboard)
+copyPixButton.addEventListener("click", copyPixToClipboard);
+qrCodeImg.addEventListener("click", copyPixToClipboard);
